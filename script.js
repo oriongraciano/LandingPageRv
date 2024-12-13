93815d362ccc6d5de50293a3e52191518668ce17
Orion Graciano <87495924+oriongraciano@users.noreply.github.com>
2024-12-12 23:50:44 -0300

    Landing Page Nogueira

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 setTimeout(function(){
     Toastify({
         text: "Olá, Seja Bem Vindo!",
-        duration: 3000,
+        duration: 2000,
         close: true,
         gravity: "top", // `top` or `bottom`
         position: "center", // `left`, `center` or `right`
         stopOnFocus: true, // Prevents dismissing of toast on hover
         style: {
-            background: "linear-gradient(to right, #00A8FF, #FF0000)",
+            background: "linear-gradient(to right, #00A8FF, #e7e710)",
         },
     }).showToast();
 },400);
